9c0a3b5e0f5f09f8f0844764cd687c314f2ca2e9
Kudakwashe Mupeni <terrencemupeni@gmail.com>
2021-02-28 13:35:55 +0200

    Update graphql.js

--- pages/api/graphql.js
+++ pages/api/graphql.js
@@ -1,21 +1,43 @@
-import { ApolloServer, gql } from 'apollo-server-micro'
+import { gql } from "apollo-server-micro";
+import axios from "axios";
+
+const getStory = async (storyId) => {
+  const result = await axios.get(`${storyUrl + storyId}.json`);
+  return selectFields(result.data);
+};
+
+const getStoryIds = async () => {
+  const result = await axios.get(newStoriesUrl);
+  return result.data;
+};
 
 const typeDefs = gql`
+  type Post {
+    by: String
+    title: String
+    text: String
+    score: String
+  }
+
   type Query {
-    users: [User!]!
+    feed: [Post!]!
+    post(storyId: String!): Post
   }
-  type User {
-    name: String
-  }
-`
+`;
 
 const resolvers = {
   Query: {
-    users(parent, args, context) {
-      return [{ name: 'Nextjs' }]
+    feed: async (parent, args, ctx) => {
+      const storyIds = await getStoryIds();
+      const data = await Promise.all(storyIds.map((id) => getStory(id)));
+
+      return data;
+    },
+    post: (parent, { storyId }, ctx) => {
+      return getStory(storyId);
     },
   },
-}
+};
 
 const apolloServer = new ApolloServer({ typeDefs, resolvers })
 
